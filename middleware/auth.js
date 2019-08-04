8e8ad3979aaef2e723f07661bd52c22a46fefd0f
Patryk <patryk-romaniuk@o2.pl>
2019-08-04 17:08:40 +0200

    Made middleware to verify token

---
+++ middleware/auth.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+module.exports = function(req,res,next){
+    try {
+        const token = req.header['x-auth-token'];
+        if(!token)
+            return res.status(404).json({ msg: "Token not found" });
+        const decoded = jwt.verify(
+            token,
+            config.get('jwtSecret')
+        )
+        req.user = decoded.user;
+        next();
+    } catch (error) {
+        console.log(error.message);
+    }
+}
