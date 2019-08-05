5671d01372ac804bfcc271c22ee064ee4dc5238f
Patryk <patryk-romaniuk@o2.pl>
2019-08-05 09:51:44 +0200

    Made Put Request in users route
    Made put request to add pizza in pizzas array

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const User = require('../modules/User');
 const bcryptjs = require('bcryptjs');
+const Pizza = require('../modules/Pizza');
 const jwt = require('jsonwebtoken');
+const auth = require('../middleware/auth');
 const config = require('config');
 const { check,validationResult } = require('express-validator');
 
@@ -49,6 +51,75 @@ router.post(
                     res.json({ token })
                 }
             );
+        } catch (error) {
+            console.log(error.message);
+            return res.status(500).json({ msg: "Server Error..." });
+        }
+    }
+);
+
+router.put(
+    '/',
+    [
+        check('name','Pizza name not found').not().isEmpty(),
+        check('price','Price not found').not().isEmpty(),
+        check('count',"Count is required").not().isEmpty()
+    ],
+    auth,
+    async(req,res) => {
+        const { name,price,count } = req.body;
+        let user = await User.findById(req.user.id);
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(500).json({ errors: errors.array() });
+        }
+        try {
+           /* let pizzaName = await Pizza.findOne({ name });
+            if(pizzaName)
+                return res.status(401).json({ msg: "There is pizza like that,just change the count." });
+            const pizza = new Pizza({
+                name,
+                price,
+                count
+            });*/
+            const pizzaItem = {
+                pizzaName: name,
+                pizzaPrice: price,
+                pizzaCount: count
+            };
+            user.pizzas.unshift(pizzaItem)
+
+            await user.save();
+            res.json(user);
+        } catch (error) {
+            console.log(error.message);
+            return res.status(400).json({ msg: "Server Error" });
+        }
+    }
+);
+
+router.get(
+    '/me',
+    auth,
+    async(req,res) => {
+        try {
+            let user = await User.findById(req.user.id).select('-password');
+            if(!user)
+                return res.status(404).json({ msg: "User Not Found" });
+            res.json(user);    
+        } catch (error) {
+            console.log(error.message);
+            return res.status(500).json({ msg: "Server Error..." });
+        }
+    }
+);
+
+router.get(
+    '/',
+    async(req,res) => {
+        try {
+            let users = await User.find().select('-password');
+            res.json(users);
         } catch (error) {
             console.log(error.message);
             return res.status(500).json({ msg: "Server Error..." });
