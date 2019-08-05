9c5019a84e50e50cb4241d2046d65abfb73c3e5a
Patryk <patryk-romaniuk@o2.pl>
2019-08-05 10:12:52 +0200

    Checking user (auth middleware)
    Searching for user by token from localstorage passed in x-auth-token header

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,7 @@ const config = require('config');
 
 module.exports = function(req,res,next){
     try {
-        const token = req.header['x-auth-token'];
+        const token = req.header('x-auth-token');
         if(!token)
             return res.status(404).json({ msg: "Token not found" });
         const decoded = jwt.verify(
