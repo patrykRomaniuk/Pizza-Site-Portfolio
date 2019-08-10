ddccd22889970c1982a70d392facc2ae3146ece1
Patryk <patryk-romaniuk@o2.pl>
2019-08-10 09:58:30 +0200

    Changed Landing Page

---
+++ client/src/Landing.js
@@ -0,0 +1,22 @@
+import React,{ useState } from 'react';
+import Hero from './components/Hero.js'
+import Recipes from './components/Recipes.js'
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+
+const Landing = () => {
+  const [modal,setModal] = useState(true);
+    return (
+        <div className="main-image">
+        <Navbar modal={modal} setModal={ setModal }/>
+          <Hero/>
+          <div className="phone-num-wrapper">
+          <span className="phone-num">Phone Number:<br/> +48 221 564 725</span>
+          </div>
+          <Recipes/>
+        <Footer/>
+    </div>
+    )
+}
+
+export default Landing;
