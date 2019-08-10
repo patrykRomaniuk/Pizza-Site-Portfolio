a70ead9ff8f91cfd251549199640e9ce22981422
Patryk <patryk-romaniuk@o2.pl>
2019-08-10 10:14:05 +0200

    Added Hero.js

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,10 +1,12 @@
-import React from 'react'
-import './herocss/hero.css'
+import React from 'react';
+import './herocss/hero.css';
 
 export default function Hero() {
   return (
     <div className="hero">
-        <span className="hero-logo">Dream<br/> Pizza</span>
+        <span className="hero-logo">
+          Dream<br/> Pizza
+        </span>
     </div>
   )
 }
