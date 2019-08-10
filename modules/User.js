3cd267379f1651b70abfef491ce2b35defdf00db
Patryk <patryk-romaniuk@o2.pl>
2019-08-10 12:46:27 +0200

    Modified User.js !2
    Added Starting Price to Pizzas Array

--- modules/User.js
+++ modules/User.js
@@ -12,7 +12,27 @@ let User = mongoose.Schema({
     password: {
         type: String,
         required: true
-    }
+    },
+    pizzas: [
+        {
+            pizzaName: {
+                type: String,
+                required: true
+            },
+            pizzaPrice: {
+                type: String,
+                required: true
+            },
+            pizzaCount: {
+                type: String,
+                required: true
+            },
+            priceStart: {
+                type: String,
+                required: true
+            }
+        }
+    ]
 });
 
 module.exports = User = mongoose.model('user',User);
