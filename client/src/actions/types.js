85f008968d4db98009bc30df0ccde59dcc7e8b14
Patryk <patryk-romaniuk@o2.pl>
2019-08-10 10:16:08 +0200

    Added constants for action.type

---
+++ client/src/actions/types.js
@@ -0,0 +1,13 @@
+export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
+export const REGISTER_FAIL = "REGISTER_FAIL";
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGIN_FAIL = "LOGIN_FAIL";
+export const AUTH_ERROR = "AUTH_ERROR";
+export const USER_LOADED = "USER_LOADED";
+export const LOG_OUT = "LOG_OUT";
+export const SET_PIZZA = "SET_PIZZA";
+export const REMOVE_PIZZA = "REMOVE_PIZZA";
+export const SET_MODAL = "SET_MODAL";
+export const REMOVE_MODAL = "REMOVE_MODAL";
+export const ADD_COUNT = "ADD_COUNT";
+export const ERROR_HANDLER = "ERROR_HANDLER";
