a9185231426cef2f808bf7fab1a7abbe858d95d7
Patryk <patryk-romaniuk@o2.pl>
2019-08-11 10:38:35 +0200

    Inserted use of react to express

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const cors = require('cors');
 const connectDatabase = require('./config/db');
@@ -11,6 +12,14 @@ app.use(express.json({ extended: false }));
 
 app.use('/api/users',require('./routes/users'));
 app.use('/api/auth',require('./routes/auth'));
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+
+    app.get('*',(req,res) => {
+        res.sendFile(path.resolve(__dirname,'build','index.html'));
+    });
+}
 
 const PORT = process.env.PORT || 5000;
 
