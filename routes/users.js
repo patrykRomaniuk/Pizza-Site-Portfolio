4ce66647b27d55eb820e4350ccb1331ca1d37976
Patryk <patryk-romaniuk@o2.pl>
2019-08-13 13:56:59 +0200

    Added Stripe Backend

--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,8 @@ const Pizza = require('../modules/Pizza');
 const jwt = require('jsonwebtoken');
 const auth = require('../middleware/auth');
 const config = require('config');
+const uuid = require('uuid');
+const stripe = require('stripe')('sk_test_0HYNOmPXMUaMYDpOyPccKyIo00QDMu2FKy');
 const { check,validationResult } = require('express-validator');
 
 router.post(
@@ -57,6 +59,51 @@ router.post(
         }
     }
 );
+
+router.post("/checkout", async (req, res) => {
+    console.log("Request:", req.body);
+  
+    let error;
+    let status;
+    try {
+      const { allPizzaPrices, token } = req.body;
+  
+      const customer = await stripe.customers.create({
+        email: token.email,
+        source: token.id
+      });
+      const idempotency_key = uuid.v4();
+      const charge = await stripe.charges.create(
+        {
+          amount: allPizzaPrices * 1,
+          currency: "usd",
+          customer: customer.id,
+          receipt_email: token.email,
+          description: `Purchased the Items`,
+          shipping: {
+            name: token.card.name,
+            address: {
+              line1: token.card.address_line1,
+              line2: token.card.address_line2,
+              city: token.card.address_city,
+              country: token.card.address_country,
+              postal_code: token.card.address_zip
+            }
+          }
+        },
+        {
+          idempotency_key
+        }
+      );
+      console.log("Charge:", { charge });
+      status = "success";
+    } catch (error) {
+      console.error("Error:", error);
+      status = "failure";
+    }
+  
+    res.json({ error, status });
+  });
 
 router.put(
     '/',
