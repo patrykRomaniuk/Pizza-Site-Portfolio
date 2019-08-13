7d6a083bbd5642f91412d3820b104b215adce68c
Patryk <patryk-romaniuk@o2.pl>
2019-08-13 13:53:45 +0200

    Added AllPizzaValue to buy button

---
+++ client/src/ModalWrapper.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { removeModal } from './actions/auth';
+import Modal from './components/Modal';
+import { connect } from 'react-redux';
+
+const ModalWrapper = ({ removeModal,modal,auth: { allPizzaPrices } }) => {
+    return (
+        <div className="modal-wrapper" style={{ display: modal === true ? 'flex' : 'none'}}>
+        <div className="modal-header">
+        <p className="basket">Basket</p>
+        <p className="closeModal"  onClick={() => removeModal()}>
+          <i className="fas fa-window-close closeBtn" style={{ zIndex: '2' }}>
+          </i>
+        </p> 
+      </div>
+      <div className="basket-modal">
+        <div className="main-modal"> 
+          <Modal/>
+        </div>
+        <div className="btn-modal-wrapper">
+        <Link to="/buy-items" onClick={() => {
+          removeModal();
+        }}>
+          <button className="btn-modal">
+            Buy { allPizzaPrices }$
+          </button>
+          </Link>
+        </div>
+      </div>
+      </div>
+    )
+}
+
+const mapStateToProps = state => ({
+    auth: state.auth,
+    modal: state.auth.modal
+});
+
+export default connect(mapStateToProps, { removeModal })(ModalWrapper);
