ffb6e957b8b7c7cb71dafc65df2cfa78c8cc99df
Patryk <patryk-romaniuk@o2.pl>
2019-08-22 18:49:29 +0200

    chuj

--- server.js
+++ server.js
@@ -21,6 +21,6 @@ if(process.env.NODE_ENV === 'production'){
     });
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => console.log(`App running on Port: ${PORT}`));
