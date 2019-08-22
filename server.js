c3272e68fe7e9bcb15fc00917c7fa1b5c4eaaafd
Patryk <patryk-romaniuk@o2.pl>
2019-08-22 19:11:54 +0200

    dsa

--- server.js
+++ server.js
@@ -21,6 +21,6 @@ if(process.env.NODE_ENV === 'production'){
     });
 }
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`App running on Port: ${PORT}`));
